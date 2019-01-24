2c08fbe66a90c4f8fd1799f536ae91da99991131
Jan Kammermayer <honza.kammermayer@gmail.com>
2019-01-24 19:20:14 +0100

    add api.py

---
+++ api.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from currency_converter import CurrencyConverter
+import requests
+
+app = Flask(__name__)
+
+@app.route('/currency_converter', methods=['GET'])
+def index():
+
+    input_currency = request.args.get('input_currency')
+    amount = request.args.get('amount')
+    output_currency = request.args.get('output_currency')
+
+    try:
+        converter = CurrencyConverter()
+        return converter.convert(input_currency, amount, output_currency)
+    except (ValueError, TypeError, requests.exceptions.RequestException) as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
